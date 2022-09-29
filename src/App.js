620a98b5535cd30108ec8605d0c52696b9e48c2a
chadaevw <vitalii.chadaev@grphn.ru>
2022-09-29 17:20:37 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+1111
+      </div>
+    </div>
+  );
+}
+
+export default App;
