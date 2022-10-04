e3b3969f0c2415cf1eb1444988d8db693794aab2
chadaevw <vitalii.chadaev@grphn.ru>
2022-10-04 12:51:20 +0400

    add change theme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './Header.scss';
 import Logo from "../Icons/Logo";
 import Theme from "../Icons/Theme";
 
 const Header = () => {
+
+    const [theme, setTheme] = useState('dark')
+
+    const changeTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark')
+    }
+
+    useEffect(() => {
+        const backgroundColor = `var(--background-color-${theme})`
+        const fontColor = `var(--font-color-${theme})`
+        const backgroundInput = `var(--background-input-${theme})`
+        const borderColor = `var(--border-color-${theme})`
+        const placeholderColor = `var(--placeholder-color-${theme})`
+
+        document.body.style.setProperty('--background-color', backgroundColor)
+        document.body.style.setProperty('--font-color', fontColor)
+        document.body.style.setProperty('--background-input', backgroundInput)
+        document.body.style.setProperty('--border-color', borderColor)
+        document.body.style.setProperty('--placeholder-color', placeholderColor)
+    }, [theme])
+
     return (
         <header className="header">
             <div className="header__icon-wrap">
                 {React.createElement(Logo)}
             </div>
-            <button className="button">
+            <button className="button" onClick={changeTheme}>
                 {React.createElement(Theme)}
             </button>
         </header>
